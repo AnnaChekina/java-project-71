8d8462ef77a92bc9faa5fc8608a0e2d3aa3dae62
Anna Chekina <an-na-di@yandex.ru>
2025-07-23 00:13:58 +0300

    feat: add CLI interface with picocli

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,6 +18,8 @@ repositories {
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+    implementation("info.picocli:picocli:4.7.5")
+    annotationProcessor("info.picocli:picocli-codegen:4.7.5")
 }
 
 tasks.test {
