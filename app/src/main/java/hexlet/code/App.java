badb93e6acb04da53173845905fee9488aa54ad0
Anna Chekina <an-na-di@yandex.ru>
2025-07-25 17:01:06 +0300

    feat: implement file paths and format options

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -2,16 +2,22 @@ package hexlet.code;
 
 import picocli.CommandLine;
 import picocli.CommandLine.Command;
-//import picocli.CommandLine.Option;
+import picocli.CommandLine.Option;
+import picocli.CommandLine.Parameters;
 
-@Command(
-        name = "gendiff",
-        mixinStandardHelpOptions = true,
-        version = "gendiff 1.0",
-        description = "Compares two configuration files and shows a difference."
-)
+@Command(name = "gendiff", mixinStandardHelpOptions = true, version = "gendiff 1.0",
+        description = "Compares two configuration files and shows a difference.")
 
 public class App implements Runnable {
+
+    @Parameters(index = "0", description = "path to first file", paramLabel = "filepath1")
+    private String filepath1;
+
+    @Parameters(index = "1", description = "path to second file", paramLabel = "filepath2")
+    private String filepath2;
+
+    @Option(names = {"-f", "--format"}, description = "output format [default: stylish]", paramLabel = "format")
+    private String format;
 
     @Override
     public void run() {
