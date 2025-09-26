b93145a648a883feba6fc1b97b43fd3b3438181e
Anna Chekina <an-na-di@yandex.ru>
2025-09-27 00:52:35 +0300

    refactor: simplify JSON formatter using direct serialization

--- app/src/main/java/hexlet/code/formatters/Json.java
+++ app/src/main/java/hexlet/code/formatters/Json.java
@@ -5,10 +5,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import hexlet.code.DiffItem;
 
-import java.util.ArrayList;
-import java.util.LinkedHashMap;
 import java.util.List;
-import java.util.Map;
 
 public class Json {
 
@@ -16,31 +13,6 @@ public class Json {
         ObjectMapper mapper = new ObjectMapper();
         mapper.enable(SerializationFeature.INDENT_OUTPUT);
 
-        List<Map<String, Object>> result = new ArrayList<>();
-
-        for (DiffItem item : diff) {
-            Map<String, Object> itemMap = new LinkedHashMap<>();
-            itemMap.put("key", item.getKey());
-            itemMap.put("status", item.getStatus());
-
-            switch (item.getStatus()) {
-                case "same", "removed":
-                    itemMap.put("oldValue", item.getValue1());
-                    break;
-                case "updated":
-                    itemMap.put("oldValue", item.getValue1());
-                    itemMap.put("newValue", item.getValue2());
-                    break;
-                case "added":
-                    itemMap.put("newValue", item.getValue2());
-                    break;
-                default:
-                    break;
-            }
-
-            result.add(itemMap);
-        }
-
-        return mapper.writeValueAsString(result);
+        return mapper.writeValueAsString(diff);
     }
 }
